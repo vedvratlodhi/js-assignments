bd53cf103d72a36fe888878d5e7cf5595cf335ba
Vedvrat Lodhi <vedvrat_lodhi@epam.com>
2019-01-29 21:48:14 +0530

    added in 3

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -234,7 +234,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-    throw new Error('Not implemented');
+    // return arr.map((x,index,c) => x+ (c[index+1]?c[index+1]:0));
 }
 
 /**
@@ -249,7 +249,7 @@ function getMovingSum(arr) {
  * [ "a" ] => []
  */
 function getSecondItems(arr) {
-    throw new Error('Not implemented');
+    return arr.filter((x,index)=>index%2);
 }
 
 
